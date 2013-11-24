e7939f74117a5591254816ee5d930e60ed6537e9
CraigCamm <craigcammarata@gmail.com>
2013-11-24 16:26:52 -0500

    add story options

--- madlibs.rb
+++ madlibs.rb
@@ -1,6 +1,12 @@
 
-story = "I had a ((an adjective)) sandwich for lunch today.  It dripped all
+stories = []
+stories << "I had a ((an adjective)) sandwich for lunch today.  It dripped all
      over my ((a body part)) and ((a noun))."
+stories << "Our favorite language is ((gem:a gemstone)).  We think ((gem)) is
+     better than ((a gemstone))."
+
+print "Pick a story [1-" + stories.count.to_s + "]: "
+story = stories[gets.chomp().to_i - 1]
 
 swap_list = story.scan(/\(\((.*?)\)\)/)
 
