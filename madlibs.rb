286571e04709761cf167b6178217b8f4f08a94aa
CraigCamm <craigcammarata@gmail.com>
2013-11-24 14:01:48 -0500

    start

---
+++ madlibs.rb
@@ -0,0 +1,9 @@
+
+story = "I had a ((an adjective)) sandwich for lunch today.  It dripped all
+     over my ((a body part)) and ((a noun))."
+
+#puts story
+
+print "Enter an adjective: "
+answer = gets.chomp()
+print answer
