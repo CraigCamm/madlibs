59a4651f94ed4a8e9f7a04eb119c0d9072055298
CraigCamm <craigcammarata@gmail.com>
2013-11-24 18:13:35 -0500

    support reusable values

--- madlibs.rb
+++ madlibs.rb
@@ -1,4 +1,3 @@
-
 stories = []
 stories << "I had a ((an adjective)) sandwich for lunch today.  It dripped all
      over my ((a body part)) and ((a noun))."
@@ -9,15 +8,25 @@ print "Pick a story [1-" + stories.count.to_s + "]: "
 story = stories[gets.chomp().to_i - 1]
 
 swap_list = story.scan(/\(\((.*?)\)\)/)
-
+stored_answers = []
+is_hash = FALSE
 swap_list.each do |item|
-    print "Enter " + item[0] + ":"
-    story = story.sub("(("+item[0]+"))", gets.chomp())
+    
+    placeholder = item[0]
+
+    if stored_answers.include?(placeholder)
+        replacement = stored_answers[placeholder]
+    elsif placeholder.include? ":"
+        placeholder_parts =  placeholder.split(':')
+        print "Enter " + placeholder_parts[1] + ": "
+        replacement = gets.chomp()
+        stored_answers = {placeholder_parts[0] => replacement}
+    else
+        print "Enter " + placeholder + ": "
+        replacement = gets.chomp()
+    end
+
+    story = story.sub("(("+placeholder+"))", replacement)
 end
     
 puts story
-
-
-#print "Enter an adjective: "
-#answer = gets.chomp()
-#print answer
