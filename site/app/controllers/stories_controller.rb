e36c0a3d978717b97bc9b78293bd3ea2e3317584
CraigCamm <craigcammarata@gmail.com>
2013-11-24 21:33:25 -0500

    Story CRUD

--- site/app/controllers/stories_controller.rb
+++ site/app/controllers/stories_controller.rb
@@ -1,2 +1,49 @@
 class StoriesController < ApplicationController
+
+    def index
+        @stories = Story.all
+    end
+
+    def update
+        @story = Story.find(params[:id])
+        if @story.update(params[:story].permit(:name, :text))
+            redirect_to @story
+        else
+            render 'edit'
+        end
+    end
+
+    def new
+        @story = Story.new
+    end
+
+    def create
+        @story = Story.new(story_params)
+        
+        if @story.save
+            redirect_to @story
+        else
+            render 'new'
+        end
+    end
+    
+    def show
+        @story = Story.find(params[:id])
+    end
+
+    def edit 
+        @story = Story.find(params[:id])
+    end
+
+    def destroy
+        @story = Story.find(params[:id])
+        @story.destroy
+
+        redirect_to stories_path
+    end
+
+    private
+        def story_params
+            params.require(:story).permit(:name, :text)
+        end 
 end
--- site/app/models/story.rb
+++ site/app/models/story.rb
@@ -1,2 +1,3 @@
 class Story < ActiveRecord::Base
+    validates :name, presence: true, length: {minimum:5}
 end
