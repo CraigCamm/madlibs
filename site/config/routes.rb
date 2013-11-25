6ca213216cdc8a983a70a7da16da83403908fa48
CraigCamm <craigcammarata@gmail.com>
2013-11-24 21:11:43 -0500

    controller/model basics

--- site/config/routes.rb
+++ site/config/routes.rb
@@ -1,4 +1,5 @@
 Site::Application.routes.draw do
+  get "intro/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
